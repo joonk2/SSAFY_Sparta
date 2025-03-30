2d6f3a08e6134b4ce6d1ed7a02dbaa71d4eb4b98
joonk2 <with.joonk@gmail.com>
2025-03-31 01:55:41 +0900

    swea_19094_화물도크

--- ps_0313/swea_19094_화물도크.py
+++ ps_0313/swea_19094_화물도크.py
@@ -1,23 +1,22 @@
 T = int(input())
-
-for tc in range(1, T + 1):
+for tc in range(1, T+1):
     N = int(input())
-    workHour = []
-
+    work_hour = []
     for _ in range(N):
-        s, e = list(map(int, input().split()))
-        workHour.append((s, e))
+        start, end = map(int, input().split())
+        work_hour.append( (start, end) )
+    
+    # work_hour asc, based on "end_time"
+    work_hour.sort(key = lambda x : x[1])
 
-    # 업무시간을 끝나는 시간으로 오름차순 정렬
-    workHour.sort(key=lambda x: x[1])
-
-    # ----- 비교 -----
-    # start가 시작시간보다 작으면 start를 끝내는 시간으로 최신화
-    start = 0;
-    cnt = 0;
-    for w in workHour:
-        if start <= w[0]:
+    # cum_decrease
+    # (start renew end)
+    start = 0
+    cnt = 0
+    for w in work_hour:
+        if (start <= w[0]):
             start = w[1]
             cnt += 1
 
-    print(f'#{tc} {cnt}')
+    print(f"#{tc} {cnt}")
+    # print(work_hour)
