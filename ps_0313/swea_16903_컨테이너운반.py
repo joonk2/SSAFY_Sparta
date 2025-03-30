52358dd76f5831ef7008e5cc7d5cc847491ffabf
joonk2 <with.joonk@gmail.com>
2025-03-31 01:15:01 +0900

    swea_16903_컨테이너운반

--- ps_0313/swea_16903_컨테이너운반.py
+++ ps_0313/swea_16903_컨테이너운반.py
@@ -1,31 +1,26 @@
 """
-[PS]
-swea - 16903 (컨테이너 운반)
-
-[what are we supposed to get?]
-- maxWeight
-
 [idea]
-1. make all the list desc to compare from the max_val
-2. if weight is included in capacity[i], then sum += (weight[i])
+-1. two options (weight, capacity of truck)
+-2. make arr desc
+-3. let's say there's 5kg of items and 8kgs of capacity
+-4. it should be like this --> [1,1,1,1,1, , , ,]
+-5. based on this way to calculate, we get to the solution of maximum weight that we can handle
 """
 
 T = int(input())
 for tc in range(1, T+1):
-    N,M = list(map(int, input().split()))
-
+    N, M = map(int, input().split())
     weight = list(map(int, input().split()))
     capacity_of_truck = list(map(int, input().split()))
 
-    # compare from the max_val
     weight.sort(reverse=True)
     capacity_of_truck.sort(reverse=True)
 
     idx = 0
-    maxWeight = 0
+    max_weight = 0
     for w in weight:
         if (idx < M and w <= capacity_of_truck[idx]):
-            maxWeight += w
+            max_weight += w
             idx += 1
-
-    print(f'#{tc} {maxWeight}')
+        
+    print(f"#{tc} {max_weight}")
