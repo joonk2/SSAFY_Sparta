e725266a7ea7fdf2e2718138e93446bc1d3a75cf
joonk2 <with.joonk@gmail.com>
2025-05-08 22:16:19 +0900

    swea 5248 그룹나누기

--- ps_0506/swea_5248_그룹나누기.py
+++ ps_0506/swea_5248_그룹나누기.py
@@ -1,34 +1,76 @@
-def find(x):
-    if parents[x] != x:
-        parents[x] = find(parents[x])
-    return parents[x]
+# def find(x):
+#     if parents[x] != x:
+#         parents[x] = find(parents[x])
+#     return parents[x]
+
+# def union(x,y):
+#     boss_x = find(x)
+#     boss_y = find(y)
+#     if boss_x != boss_y:
+#         parents[boss_y] = boss_x
+
+
+
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, M = map(int, input().split())
+#     data = list(map(int, input().split()))
+    
+#     # parents = []
+#     # for i in range(1, N+1):
+#     #     parents.append(i)
+#     parents = [i for i in range(N+1)]
+#     # print(parents)
+
+#     for i in range(0, M*2, 2):
+#         a = data[i]
+#         b = data[i+1]
+#         union(a,b)
+
+#     group = set()
+#     for i in range(1, N+1):
+#         group.add(find(i))
+
+#     print(f"#{tc} {len(group)}")
+
+
+
+
+
+
+
+
+
+
+
+def find_set(x):
+    while rep[x] != x:
+        x = rep[x]
+    return x
 
 def union(x,y):
-    boss_x = find(x)
-    boss_y = find(y)
-    if boss_x != boss_y:
-        parents[boss_y] = boss_x
+    rep[find_set(y)] = find_set(x)
 
 
 
 T = int(input())
 for tc in range(1, T+1):
-    N, M = map(int, input().split())
-    data = list(map(int, input().split()))
-    
-    # parents = []
-    # for i in range(1, N+1):
-    #     parents.append(i)
-    parents = [i for i in range(N+1)]
-    # print(parents)
+    N, M = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
 
-    for i in range(0, M*2, 2):
-        a = data[i]
-        b = data[i+1]
-        union(a,b)
+    rep = [i for i in range(N+1)]
 
-    group = set()
+    # n1, n2 입력후 합집합 형성
+    for i in range(M):
+        n1 = arr[i*2]
+        n2 = arr[i*2 + 1]
+        union(n1, n2)
+
+
+    # i가 어떤 집합의 대표인지 찾기
+    cnt = 0
     for i in range(1, N+1):
-        group.add(find(i))
+        if rep[i] == i:
+            cnt += 1
 
-    print(f"#{tc} {len(group)}")
+    print(f"#{tc} {cnt}")
