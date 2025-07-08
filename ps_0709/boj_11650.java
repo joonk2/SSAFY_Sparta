05748768ce7d5fc40be2c180d214ea27d4f2a74e
DESKTOP-DLOPF19\SSAFY <with.joonk@gmail.com>
2025-07-09 08:54:24 +0900

    람다식 정렬 --> boj_11650_좌표정렬하기.java

---
+++ ps_0709/boj_11650.java
@@ -0,0 +1,48 @@
+/*
+람다식 비교 (오름차순 정렬) 
+*/
+
+package boj;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.util.Arrays;
+
+public class boj_11650_좌표정렬하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		// x,y 좌표를 저장할 new int[N][2]
+		int[][] pos = new int[N][2];
+		
+		
+		for (int i = 0; i < N; i++) {
+			String[] xy = br.readLine().split(" ");
+			int x = Integer.parseInt(xy[0]);
+			int y = Integer.parseInt(xy[1]);
+			pos[i][0] = x;
+			pos[i][1] = y;
+		}
+		
+		
+		// lambda 비교 (asc)
+		Arrays.sort(pos, (a, b) -> {
+			if (a[0] == b[0]) {
+				return Integer.compare(a[1], b[1]);
+			}
+			return Integer.compare(a[0], b[0]);
+		});
+		
+		
+		// res
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append(pos[i][0] + " " + pos[i][1] + "\n");
+		}
+		System.out.println(sb.toString());
+		
+	}
+}
