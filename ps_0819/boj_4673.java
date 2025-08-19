dacf5132ea2892b97abee42d4f2ff1e009b63423
DESKTOP-DLOPF19\SSAFY <with.joonk@gmail.com>
2025-08-19 17:05:04 +0900

    boj_4673_셀프넘버 --> implemantation

--- ps_0819/boj_4673.java
+++ ps_0819/boj_4673.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-public class Main {
+public class boj_4673_셀프넘버 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		boolean[] generated = new boolean[10001];
