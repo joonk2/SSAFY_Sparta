bc5d8dd4fa9b11fa00873cfc999f4308db8f2156
joonk2 <with.joonk@gmail.com>
2025-09-02 07:45:07 +0900

    swea_1949_등산로조성 --> back tracking

--- ps_0902/swea_1949_등산로조성.java
+++ ps_0902/swea_1949_등산로조성.java
@@ -1,8 +1,9 @@
 package swea;
 
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 
 
 public class swea_1949_등산로조성 {
@@ -12,8 +13,9 @@ public class swea_1949_등산로조성 {
 	static boolean[][] visited;
 	static int[] dr = {-1,1,0,0};
 	static int[] dc = {0,0,-1,1};
-	static int max_height;
+	
 	static int max_leng;
+	static int max_height;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,10 +24,11 @@ public class swea_1949_등산로조성 {
 			String[] NK = br.readLine().split(" ");
 			N = Integer.parseInt(NK[0]);
 			K = Integer.parseInt(NK[1]);
+			
 			matrix = new int[N][N];
 			visited = new boolean[N][N];
-			max_leng = 1;
 			max_height = 0;
+			max_leng = 1;
 			
 			for (int r = 0; r < N; r++) {
 				String[] cols = br.readLine().split(" ");
@@ -53,19 +56,26 @@ public class swea_1949_등산로조성 {
 	
 	
 	
-	static void dfs(int r, int c, int cut_used, int cur_leng) {
+	
+	static void dfs(int r, int c, int whether_cut_used, int cur_leng) {
 		for (int d = 0; d < 4; d++) {
-			if (max_leng < cur_leng) max_leng = cur_leng;
+			if (cur_leng > max_leng) max_leng = cur_leng;
+			
 			int nr = r + dr[d];
 			int nc = c + dc[d];
-			if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc]) {
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N && ! visited[nr][nc]) {
+				// 조건 2개
+				// 1. cur_height > next_height
+				// 2. cur_height <= next_height
+				// 2-1. 길을 깎지 않았고 && cur_height > next_height-K
+				// next_height를 잠시 복사해놓고, 길을 깎고 dfs 순회후 다시 복구시키자
 				if (matrix[r][c] > matrix[nr][nc]) {
 					visited[nr][nc] = true;
-					dfs(nr, nc, cut_used, cur_leng+1);
+					dfs(nr, nc, whether_cut_used, cur_leng+1);
 					visited[nr][nc] = false;
 				}
 				else if (matrix[r][c] <= matrix[nr][nc]) {
-					if (cut_used == 1 && matrix[r][c] > matrix[nr][nc] - K) {
+					if (whether_cut_used == 1 && matrix[r][c] > matrix[nr][nc] - K) {
 						int original = matrix[nr][nc];
 						matrix[nr][nc] = matrix[r][c]-1;
 						visited[nr][nc] = true;
@@ -77,6 +87,8 @@ public class swea_1949_등산로조성 {
 			}
 		}
 	}
+	
+	
 	
 	
 	
