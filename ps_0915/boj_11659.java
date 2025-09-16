861202b56132a9f86c0f38aced2c002cdc76f756
joonhwan.K <with.joonk@gmail.com>
2025-09-17 01:07:49 +0900

    Update boj_11659.java

--- ps_0915/boj_11659.java
+++ ps_0915/boj_11659.java
@@ -1,58 +1,65 @@
-/*
-해당문제는 java로 풀면 1초라서 1억 연산 이내가 되야한다.
-나는 두번째에 성공했는데, 첫번째에서는 2중 for문을 사용했어서 최악의 연산시간은 다음과 같았다
-100,000 + 1 + (99,999) + (100,000 * 1000) 
-1억이 넘기에 당연히 시간초과했다.
-
-2번째에서는 O(N+M) 이 되도록 조정했다.
-*/
-
-package boj;
-
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import java.util.Arrays;
 
-public class boj_11659_구간합구하기4 {
+public class Main {
 	static int N, M;
-	static String[] arr;
-	static int[] A, S;
-	
+	static int[][] A, S;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] NM = br.readLine().split(" ");
 		N = Integer.parseInt(NM[0]);
 		M = Integer.parseInt(NM[1]);
-		arr = br.readLine().split(" ");
-		A = new int[N];
-		S = new int[N];
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(arr[i]);
+		A = new int[N][N];
+		for (int r = 0; r < N; r++) {
+			String[] cols = br.readLine().split(" ");
+			for (int c = 0; c < N; c++) {
+				A[r][c] = Integer.parseInt(cols[c]);
+			}
 		}
 		
+
 		// prefix_sum
-		S[0] = A[0];
-		for (int i = 1; i < N; i++) {
-			S[i] = S[i-1] + A[i];
+		S = new int[N][N];
+		for (int r = 0; r < N; r++) {
+			S[r][0] = A[r][0];
+			for (int c = 1; c < N; c++) {
+				S[r][c] = S[r][c-1] + A[r][c];
+			}
 		}
 		
-		// res
+
+		
 		for (int i = 0; i < M; i++) {
-			String[] start_end = br.readLine().split(" ");
-			int start = Integer.parseInt(start_end[0]);
-			int end = Integer.parseInt(start_end[1]);
-			start = start-1;
-			if (start - 1 < 0) {
-				System.out.println(S[end-1]);
-			}
-			else if (start -1 >= 0) {
-				System.out.println(S[end-1] - S[start-1]);
-			}
+			String[] x1_y1_x2_y2 = br.readLine().split(" ");
+			int x1 = Integer.parseInt(x1_y1_x2_y2[0]);
+			int y1 = Integer.parseInt(x1_y1_x2_y2[1]);
+			int x2 = Integer.parseInt(x1_y1_x2_y2[2]);
+			int y2 = Integer.parseInt(x1_y1_x2_y2[3]);
+			x1 = x1-1;
+			y1 = y1-1;
+			x2 = x2-1;
+			y2 = y2-1;
 			
+			int sum = 0;
+			if (y1-1 < 0) {
+				for (int j = x1; j < x2 + 1 ; j++) {
+					sum += S[j][y2];
+				}
+			}
+			else if (y1-1 >= 0) {
+				for (int j = x1; j < x2 + 1; j++) {
+					sum += (S[j][y2] - S[j][y1-1]);
+				}
+			}
+			System.out.println(sum);
 		}
+		
+		
+		
+		
 		
 		
 		
